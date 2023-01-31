bd7c451383d5a4f7846d9fde6b347f5f1775ee65
Zhongtao Hu <hzt731tim@outlook.com>
2023-01-31 22:46:26 +0800

    firt commit
    Signed-off-by: Zhongtao Hu <hzt731tim@outlook.com>

--- cmd/plugin/cli/root.go
+++ cmd/plugin/cli/root.go
@@ -3,77 +3,27 @@ package cli
 import (
 	"errors"
 	"fmt"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/github/kubectl-kata/pkg/logger"
-	"github.com/github/kubectl-kata/pkg/plugin"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/tj/go-spin"
-	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"os"
 )
 
-var (
-	KubernetesConfigFlags *genericclioptions.ConfigFlags
-)
+var cmd = &cobra.Command{
+	Use:           "kata",
+	Short:         "A tool perform kata containers management",
+	SilenceErrors: true,
+	SilenceUsage:  true,
+	PreRun: func(cmd *cobra.Command, args []string) {
+		viper.BindPFlags(cmd.Flags())
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := errors.New("a subcommand need to be specified")
+		return err
+	},
+}
 
 func RootCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:           "kata",
-		Short:         "",
-		Long:          `.`,
-		SilenceErrors: true,
-		SilenceUsage:  true,
-		PreRun: func(cmd *cobra.Command, args []string) {
-			viper.BindPFlags(cmd.Flags())
-		},
-		RunE: func(cmd *cobra.Command, args []string) error {
-			log := logger.NewLogger()
-			log.Info("")
-
-			s := spin.New()
-			finishedCh := make(chan bool, 1)
-			namespaceName := make(chan string, 1)
-			go func() {
-				lastNamespaceName := ""
-				for {
-					select {
-					case <-finishedCh:
-						fmt.Printf("\r")
-						return
-					case n := <-namespaceName:
-						lastNamespaceName = n
-					case <-time.After(time.Millisecond * 100):
-						if lastNamespaceName == "" {
-							fmt.Printf("\r  \033[36mSearching for namespaces\033[m %s", s.Next())
-						} else {
-							fmt.Printf("\r  \033[36mSearching for namespaces\033[m %s (%s)", s.Next(), lastNamespaceName)
-						}
-					}
-				}
-			}()
-			defer func() {
-				finishedCh <- true
-			}()
-
-			if err := plugin.RunPlugin(KubernetesConfigFlags, namespaceName); err != nil {
-				return errors.Unwrap(err)
-			}
-
-			log.Info("")
-
-			return nil
-		},
-	}
-
 	cobra.OnInitialize(initConfig)
-
-	KubernetesConfigFlags = genericclioptions.NewConfigFlags(false)
-	KubernetesConfigFlags.AddFlags(cmd.Flags())
-
-	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
 	return cmd
 }
 
